79f9c79ce7739bab89e676e85c50884188cd7f3c
Durvesh <durveshbhole9@gmail.com>
2018-09-02 21:24:07 +0000

    addded routed for home page and usersList

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app = Flask(__name__,template_folder='/site/mantener/html/')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "/site/mantener/db/mantener")
 
+
 def is_json(request):
     if not request.json:
         print 'Not a JSON'
@@ -26,12 +27,15 @@ def is_json(request):
         return False
     return True
 
+#DB Connection
 def connect_db():
     return sqlite3.connect(DATABASE)
+
 
 @app.before_request
 def before_request():
     g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
@@ -39,6 +43,8 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+
+#Data handling API's
 @app.route("/getData")
 def geData():
     cursor = g.db.execute("SELECT * FROM work_log")
@@ -46,6 +52,7 @@ def geData():
     output = json.dumps(empData)
     print output
     return output
+
 
 @app.route("/api/v1/getUsers",methods = ['GET'])
 def getUsers():
@@ -63,6 +70,7 @@ def getUsers():
         return None
     else:
         return False
+
 
 @app.route('/api/v1/storeData',methods = ['POST'])
 def storeData():
@@ -89,6 +97,25 @@ def storeData():
 
     return json.dumps("Data Pushed Successfully.")
 
+
+@app.route('/api/v1/onCall',methods = ['GET'])
+def onCall():
+    try:
+        level = request.args.get('level')
+    except Error as e:
+        print "No parameter supplies",e
+
+    cursor = g.db.execute('select u.user_id, u.full_name, u.email, u.contact, o.level, o.team from users u join on_call_now o using(user_id) where level = ?',(level,))
+    items = []
+    headers = ['user_id','full_name','email','contact','level','team']
+    onCall = cursor.fetchall()
+    for row in onCall:
+        items.append({'user_id':row[0],'full_name':row[1],'email':row[2],'contact':row[3],'level':row[4],'team':row[5]})
+
+    return json.dumps(items)
+
+
+#Sample API
 @app.route('/foo', methods=['POST']) 
 def foo():
     if not request.json:
@@ -96,14 +123,14 @@ def foo():
     print request.json
     return json.dumps(request.json)
 
-@app.route('/')
+#Routes
+@app.route('/update')
 def index():
     return render_template('store_data.html') 
 
-# Coming Soon...
-@app.route('/view')
+@app.route('/home')
 def view():
-    return render_template('viewLog.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
    print "Opened database successfully";
